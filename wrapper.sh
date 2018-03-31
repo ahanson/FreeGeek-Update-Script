0cd0c1c71dbc9e317b5e49b9e4faf93903dcae50
Alex Hanson <ahanson@runbox.com>
2018-03-31 18:57:48 -0500

    Update and rename wrapper.txt to wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+dpkg --get-selections >before.txt
 wget https://raw.githubusercontent.com/ahanson/FreeGeek-Update-Script/master/install.sh
 chmod +x install.sh
 bash ./install.sh
 rm install.sh
+dpkg --get-selections >before.txt
+diff -y before.txt after.txt > comparison.txt
