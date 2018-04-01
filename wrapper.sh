4659f9d653689571671c6e2d04044b773388d674
Alex Hanson <ahanson@runbox.com>
2018-03-31 19:28:25 -0500

    Update wrapper.sh
    Remove the diff (since it's part of the main script).

--- wrapper.sh
+++ wrapper.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-dpkg --get-selections >before.txt
 wget https://raw.githubusercontent.com/ahanson/FreeGeek-Update-Script/master/install.sh
 chmod +x install.sh
 bash ./install.sh
-rm install.sh
-dpkg --get-selections >before.txt
-diff -y before.txt after.txt > comparison.txt
